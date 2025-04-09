56e4cf5690fbce07f203fac8b92fcb1214cba9e6
Haakon H <hhaus532@iastate.edu>
2025-04-09 15:07:30 -0500

    server setup function is done but untested

--- server/server.c
+++ server/server.c
@@ -2,7 +2,16 @@
 
 int setupServer(Server *s){
   //get socket
+  if(s->socket = socket(s->domain, s->type, s->protocal) < 0){
+    printf("FATAL: Failed to get a socket!! \n");
+    return -1;
+  }
+
   //bind socket
+  if(bind(s->socket, (struct sockaddr *) &s->server_address, sizeof(s->server_address)) < 0){
+    printf("FATAL: Failed to get a bind!! \n");
+    return -1;
+  }
 }
 
 int luachServer(Server *s){
--- server/server.h
+++ server/server.h
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
 typedef struct Server{
+struct sockaddr server_address;
 int type;
 int domain;
 int protocal;
